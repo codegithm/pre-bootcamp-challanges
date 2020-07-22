eef0dfd91d26a5bce09a26d71873ee93b620066f
codegithm <xakazantando@gmail.com>
2020-07-22 14:09:42 -0700

    Task 2

---
+++ Task2.js
@@ -0,0 +1,13 @@
+// declare
+var x = 1 + 1 * 2;
+var y = (1 + 1) * 2;
+var z = 1 + ( 1 * 2 );
+var a =  1 + 1 * 2 / 2;
+var b =  (1 + 1 * 2 ) /  2;
+
+// log the results
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
