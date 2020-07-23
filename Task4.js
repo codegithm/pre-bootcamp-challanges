52251b00196c7b4ff65a29de66f8c8a4b83f5861
codegithm <xakazantando@gmail.com>
2020-07-22 21:48:54 -0700

    Tast 4

---
+++ Task4.js
@@ -0,0 +1,19 @@
+//declare function
+function taskFunction (num1,num2){
+    var sum = num1 + num2;
+    
+    //change sum to array
+    var arrSum = sum.toString().split('');
+    
+    //find three in the array
+    var findThree = arrSum.indexOf('3');
+    
+      if(num1 === 3 || num2 === 3 && findThree >= 0 ){
+        return true
+      }else{
+        return false
+      }
+    };
+    
+    // print test
+    console.log(taskFunction(10,3));
