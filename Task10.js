9799506a6b614e9d4c63404627b9132109c3b3a4
codegithm <xakazantando@gmail.com>
2020-07-24 15:30:09 -0700

    Task 10

---
+++ Task10.js
@@ -0,0 +1,14 @@
+function findVowels(word){
+    let vowels = ['a','e','i','o','u'];
+    let strWord = word.toLowerCase().split('')
+    let vowelsFound = '';
+
+    for(let i = 0;i < vowels.length; i++){
+        if(strWord.indexOf(vowels[i]) >= 0){
+           vowelsFound += vowels[i]
+        }
+    }
+    return vowelsFound
+}
+
+console.log(findVowels('Andile'));
