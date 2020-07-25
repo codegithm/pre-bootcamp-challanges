d71ec23abff397d11a5cfe261617d17b94a26d91
codegithm <xakazantando@gmail.com>
2020-07-24 17:50:55 -0700

    Changes on Task 10

--- Task10.js
+++ Task10.js
@@ -1,15 +1,15 @@
 function findVowels(word){
     let vowels = ['a','e','i','o','u'];
     let strWord = word.toLowerCase().split('')
-    let vowelsFound = '';
+    let vowelsFound = [];
 
     for(let i = 0;i < vowels.length; i++){
         if(strWord.indexOf(vowels[i]) >= 0){
-           vowelsFound += vowels[i]
+           vowelsFound.push(vowels[i])
         }
     }
     return vowelsFound
-}
+};
 
 //For Testing Purpose
 console.log(findVowels('Andile'));
