d2eed471d3ff25a7189804aaa5cc850f03534951
codegithm <xakazantando@gmail.com>
2020-07-24 17:45:14 -0700

    Task 11

---
+++ Task11.js
@@ -0,0 +1,17 @@
+function matchedLetters(word1,word2){
+
+    let firstArr = word1.toLowerCase().split('');
+    let secArr = word2.toLowerCase().split('');
+    let match = [];
+
+    for(let i = 0; i<firstArr.length; i++){
+        if(secArr.indexOf(firstArr[i]) >= 0){
+           match.push(firstArr[i]); 
+        }
+    }
+
+    return match
+}
+
+//For testing purpose
+console.log(matchedLetters('mpendulo','andile'));
