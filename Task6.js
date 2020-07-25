0afe93f043fd5be84e928970923d36971cb32ab4
codegithm <xakazantando@gmail.com>
2020-07-24 18:00:13 -0700

    Added a function that takes as many numbers

--- Task6.js
+++ Task6.js
@@ -11,3 +11,19 @@ function findMax(a,b,c) {
 
 //For Testing Purpose
 console.log(findMax(2,7,6));
+
+
+//Bonus: How can you change the code so it can take in any number of numbers?
+function findMax2() {
+    let arrOfNums2 = Array.from(arguments);        //construct an Array from the arguments
+    let maxNum2 = 0;
+    for(let i = 0; i < arrOfNums2.length; i++){
+        if(arrOfNums2[i]>maxNum2){
+            maxNum2 = arrOfNums2[i]
+        }
+    }
+    return maxNum2
+};
+
+//For Testing Purpose
+console.log(findMax2(2,7,6,6,8,2,9,14));
